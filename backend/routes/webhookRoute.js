33e52fc0171eabc87a45cba448c88fe306907168
Uygar Deniz ACAR <uygar.d.acar@gmail.com>
2024-03-09 01:25:22 +0300

    fixed controller path on webhookRouter

--- backend/routes/webhookRoute.js
+++ backend/routes/webhookRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import webhook from "../controllers/webhookController";
+import webhook from "../controllers/webhookController.js";
+import bodyParser from "body-parser";
 const router = express.Router();
 
 router.post("/", bodyParser.raw({ type: "application/json" }), webhook);
